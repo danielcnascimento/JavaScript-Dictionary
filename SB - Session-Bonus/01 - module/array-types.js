9751a3ad659aa6841d6ccdb6f33d9e8f0b417103
danielcnascimento <ddaniel.conceicao@gmail.com>
2020-11-21 18:34:05 -0300

    🤗 adding bonuses (most used in frameworks)

---
+++ SB - Session-Bonus/01 - module/array-types.js
@@ -0,0 +1,25 @@
+
+
+// >>>> Module: thats how we can export the functions we want to make, we use "export" 
+
+
+
+
+export function pushArray(...arr){ //im using rest to say that my arguments will be an array
+
+    let newArray = [];
+
+    arr.forEach(element => {
+       newArray.push(element);
+    });
+
+    return newArray;
+
+}
+
+export function makeItAnArray(string){ //now i will take an string and make it an array, lets see:
+
+    let newArray = string.split(" "); // im saying that " " blanck space will interpolate and make me an array
+
+    return newArray;
+}
---
+++ SB - Session-Bonus/01 - module/script.js
@@ -0,0 +1,5 @@
+import {pushArray, makeItAnArray} from './array-types.js'; // We use "import" and call that file which our functions are!
+                                                           // you will see it in React or Vue .. always.
+
+console.log(pushArray(0, 1, 2, 3, 4, 5));
+console.log(makeItAnArray("As i said, I am a developer!"));
