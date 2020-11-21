1419e0c73f4c5c6e9df067093da5d79d527607de
danielcnascimento <ddaniel.conceicao@gmail.com>
2020-11-21 17:54:11 -0300

    🤨 more comments on Rest and Spread operators.

--- S3 - arrays-and-objects/17RestOperator/script.js
+++ S3 - arrays-and-objects/17RestOperator/script.js
@@ -1,11 +1,10 @@
-// Tips: to undertand it, follow the sequency please 😤 !!
+// Tips: to undertand it, follow the sequency please 😤 you will use it a lot in React or Vue !!
 
 function tornaArray(company, ...custumers){ //1° you can see the "rest" when we receive elements 🤨
     console.log(company, custumers);
 }
 
 tornaArray('Instagram', 'Steve J.', 'Mark Z.', 'Others');//the company name + clients;
-
 
 
 //Spread operator is useful when you wanna you Hooks on React.. you can "spread" values into an array for example:
