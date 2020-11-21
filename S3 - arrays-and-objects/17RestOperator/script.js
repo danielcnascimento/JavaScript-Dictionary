003b8557f570eb3d232ca4ecf8f2f036dba6ccf1
danielcnascimento <ddaniel.conceicao@gmail.com>
2020-11-21 17:48:50 -0300

    🤨 adding spread operator.

--- S3 - arrays-and-objects/17RestOperator/script.js
+++ S3 - arrays-and-objects/17RestOperator/script.js
@@ -1,19 +1,18 @@
-// to undertand it, follow the sequency please 😤 !!
+// Tips: to undertand it, follow the sequency please 😤 !!
 
-
-function tornaArray(...args){ //1° lets make a function ! the "..." are the RestOperator
-                              // this will receive a sequency of params 🤨
-
-    let newArr = [] // 💡 declaring the array to receive the params above
-
-    for(let arg of args){ // 😉 yep its a new way to make for statment ! we should use it more often 🤔.
-
-        newArr.push(arg) // do you remember push function ? lets practice it to push elements to a new array, but since its push, the last element will be the first on the new array.
-                         
-    }
-    
-    console.log(newArr)// showing the inverted array, comparing to the one above 👇
-
+function tornaArray(company, ...custumers){ //1° you can see the "rest" when we receive elements 🤨
+    console.log(company, custumers);
 }
 
-tornaArray(0,1,2,3,4,5,6,7,8,9); //passando parametros para o rest operator '...'
+tornaArray('Instagram', 'Steve J.', 'Mark Z.', 'Others');//the company name + clients;
+
+
+
+//Spread operator is useful when you wanna you Hooks on React.. you can "spread" values into an array for example:
+
+let arr = ['Daniel', 23, true];
+
+let person = [...arr, 'programmer', 'student'];
+
+console.log(arr);
+console.log(person);
