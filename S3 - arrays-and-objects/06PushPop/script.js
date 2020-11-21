d47291d1e014f4039489127a0f6787c76b3cccb6
danielcnascimento <ddaniel.conceicao@gmail.com>
2020-11-21 12:04:33 -0300

    💡 comments on push and pop methods

--- S3 - arrays-and-objects/06PushPop/script.js
+++ S3 - arrays-and-objects/06PushPop/script.js
@@ -1,3 +1,6 @@
+// pop will be useful when you want to ADD informations on TOP of an array. Push will add info from the beginning
+//check it now 💡
+
 let arr = ['Daniel', 'Hessa', 69];
 
 let elementoRemovidoDaStack = arr.pop()
