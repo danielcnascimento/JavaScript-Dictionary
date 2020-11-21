9672987ed023ebc1ff34109b3d6b7a4c540b709d
danielcnascimento <ddaniel.conceicao@gmail.com>
2020-11-21 17:01:54 -0300

    💥more about destructuring

--- S3 - arrays-and-objects/18Destructuring/script.js
+++ S3 - arrays-and-objects/18Destructuring/script.js
@@ -7,9 +7,21 @@ let arr = [
     23,
     true
 ]
-
-let [vName, vAge, vMarried] = arr; // remeberm destructuring arrays elements into var,
+let [vName, vAge, vMarried] = arr; // remember destructuring arrays elements into var,
                                    // we have to place each var in sequence 💡 according to that destructured array:
+                                   // since its array destructuring, use [] to make new var.
+
+
+// what about destructuring properties from html to console.log them ? 
+
+function elements({clientX, clientY}){ //this are properties of mousemove
+
+    console.log(clientX, clientY);
+
+}
+
+document.documentElement.addEventListener("mousemove", elements); // everything in JS is objects! lets access HTML document (addEventListener) to access its method properties: the mousemove
+                                                                  // and destructure mousemove's properties hehehehehee.
 
 // want you to try them now: make your on, Open on Browser and check console. erase these ... 
 
